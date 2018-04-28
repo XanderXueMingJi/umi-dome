f2b09b28356b000eaebf77e6667fe4c30a66e506
XueMingJi <xueming.ji@qq.com>
2018-04-28 10:07:21 +0800

    第一个渲染

---
+++ pages/.umi/router.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/_renderRoutes';
+
+
+let Router = DefaultRouter;
+
+
+const routes = [
+  {
+    "component": require('../../../../AppData/Roaming/npm/node_modules/umi/node_modules/umi-build-dev/lib/DefaultLayout.js').default,
+    "routes": [
+      {
+        "path": "/index.html",
+        "exact": true,
+        "component": () => React.createElement(require('C:/Users/Administrator/AppData/Roaming/npm/node_modules/umi/node_modules/umi-build-dev/lib/Compiling.js').default, { route: '/index.html' })
+      },
+      {
+        "path": "/",
+        "exact": true,
+        "component": require('../index.js').default
+      }
+    ]
+  }
+];
+
+export default function() {
+  return (
+<Router history={window.g_history}>
+  <Route render={({ location }) =>
+    renderRoutes(routes, {}, { location })
+  } />
+</Router>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,4 @@
+//直接运行 umi dev   http://localhost:8000/
+
+// 在dist 运行 serve   http://localhost:5000   
+export default () => <div>Index Page, hello xmj</div>;
